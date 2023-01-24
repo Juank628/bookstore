ab01f73ac5108884e8ecc7b086c8350a76a24c44
Juank628 <jsanchez_zunino@yahoo.com>
2023-01-24 13:49:22 -0500

    configure store

---
+++ src/redux/books/books.js
@@ -0,0 +1,35 @@
+const ADD_BOOK = 'books/ADD_BOOK';
+
+const initialState = {
+  list: [
+    {
+      id: 1,
+      title: 'book 1',
+      author: 'auth 1',
+      category: 'cat 1',
+    },
+    {
+      id: 2,
+      title: 'book 2',
+      author: 'auth 2',
+      category: 'cat 2',
+    },
+    {
+      id: 3,
+      title: 'book 3',
+      author: 'auth 3',
+      category: 'cat 3',
+    },
+  ],
+};
+
+const booksReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export const addBook = (book) => ({ type: ADD_BOOK, book });
+
+export default booksReducer;
