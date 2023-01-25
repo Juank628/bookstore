20a0c26820f748edc6854bda3cfedd9cf5035e8f
Juank628 <jsanchez_zunino@yahoo.com>
2023-01-25 11:35:23 -0500

    add Under Construction message in categories page

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import categoriesReducer, { checkStatus } from '../redux/categories/categories';
 import styles from './Categories.module.css';
 
 export default function Categories() {
   const dispatch = useDispatch();
+  const { list } = useSelector((store) => store.categories);
 
   const clickHandler = () => {
     dispatch(categoriesReducer(checkStatus()));
@@ -12,6 +13,11 @@ export default function Categories() {
 
   return (
     <div className={styles.container}>
+      <ul>
+        {list.map((item) => (
+          <li key={item}>{ item }</li>
+        ))}
+      </ul>
       <button type="button" className={styles.checkButton} onClick={clickHandler}>Check status</button>
     </div>
   );
